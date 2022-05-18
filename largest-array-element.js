3e76c840c5449eb96989c519fc779a71469c948e
leon0113 <tahjib.leon399@gmail.com>
2022-05-19 05:56:29 +0600

    array

---
+++ largest-array-element.js
@@ -0,0 +1,15 @@
+function largestElement(numbers) {
+    let largest = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        if (element > largest) {
+            largest = element;
+        }
+    }
+    return largest;
+
+}
+
+const ages = [45, 2, 85, 69, 25];
+const oldest = largestElement(ages);
+console.log(oldest);
